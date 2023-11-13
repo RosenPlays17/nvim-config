e4770f60b9edb81823bc6ac94f491e8aa4f160b5
RosenPlays17 <eomwrmkq@duck.com>
2023-11-13 15:07:27 +0100

    first commit

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,30 @@
+local config = require("plugins.configs.lspconfig")
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+local lspconfig = require("lspconfig")
+
+lspconfig.csharp_ls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"cs"}
+})
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"}
+})
+lspconfig.phpactor.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"php"}
+})
+lspconfig.emmet_ls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"html"}
+})
+lspconfig.cssls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"css"}
+})
---
+++ lua/custom/init.lua
@@ -0,0 +1,10 @@
+vim.cmd([[
+  set number relativenumber
+  set guicursor=n:hor100
+  set guicursor+=i:ver100
+  set guicursor+=v:block
+  set guicursor+=c:hor100
+  set tabstop=3
+  set softtabstop=3
+  set wrap linebreak nolist
+]])
